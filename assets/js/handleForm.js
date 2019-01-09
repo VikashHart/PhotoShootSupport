811b1dfa9af5990a1da48df7f4e839387aebf4dc
VikashHart <32072791+VikashHart@users.noreply.github.com>
2019-01-09 10:31:19 -0500

    Update handleForm.js

--- assets/js/handleForm.js
+++ assets/js/handleForm.js
@@ -2,5 +2,5 @@ var form = document.querySelector(".contact-form");
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   var formData = new FormData(form);
-  window.location = `mailto:photobooth.snapandshare.help@gmail.com?subject=${formData.get('subject')}&body=${formData.get('body')}`;
+  window.location = `mailto:photoshoot.snapandshare.help@gmail.com?subject=${formData.get('subject')}&body=${formData.get('body')}`;
 })
